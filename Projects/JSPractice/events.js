3e72e9dc1e2ff66fdcf2ee51dda3483eb9623ef9
Casey Echternacht <casey.echo1@gmail.com>
2024-09-23 12:58:50 -0600

    finished the ticket

--- Projects/JSPractice/events.js
+++ Projects/JSPractice/events.js
@@ -1,7 +1,17 @@
 const hidebtn = document.getElementById('hide-btn');
 const showBtn = document.getElementById('show-btn');
+const hideAndShowBtn = document.getElementById('hide-and-show')
 const changeColorBtn = document.getElementById('change-color-btn');
 const demoHeading = document.getElementById('demo-heading');
+
+const colors = ['red', 'blue', 'orange', 'purple'];
+let colorIndex = 0;
+function changeColor() {
+    demoHeading.style.color = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+
+}
+setInterval(changeColor, 1000);
 
 //hide hedding
 hidebtn.addEventListener('click', () => {
@@ -9,9 +19,24 @@ hidebtn.addEventListener('click', () => {
 });
 
 //show heading
+showBtn.addEventListener('click', () => {
+    demoHeading.style.display = '';
+});
 
 //change heading color when clicked
 
-// toggle visibility of heading
+changeColorBtn.addEventListener('click', () => {
+    demoHeading.style.color = 'blue';
+});
 
-//auto change heading color
+// toggle visibility of heading
+hideAndShowBtn.addEventListener('click', () => {
+    if(demoHeading.style.display === '') {
+        demoHeading.style.display = 'none';
+    }
+    else {
+        demoHeading.style.display = '';
+    }
+});
+
+
