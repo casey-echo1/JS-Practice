683b20f7ead45adc46b37b5781fc5d605f722e57
Casey Echternacht <casey.echo1@gmail.com>
2024-09-23 12:34:04 -0600

    ticket 1, item 1 complete

---
+++ Projects/JSPractice/events.js
@@ -0,0 +1,17 @@
+const hidebtn = document.getElementById('hide-btn');
+const showBtn = document.getElementById('show-btn');
+const changeColorBtn = document.getElementById('change-color-btn');
+const demoHeading = document.getElementById('demo-heading');
+
+//hide hedding
+hidebtn.addEventListener('click', () => {
+    demoHeading.style.display = 'none';
+});
+
+//show heading
+
+//change heading color when clicked
+
+// toggle visibility of heading
+
+//auto change heading color
